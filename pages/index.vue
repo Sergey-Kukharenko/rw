77e3c90598a507630989ec4deb8f2b4de3932e93
Sergey Kukharenko <iamksergey@mail.ru>
2022-06-23 18:45:02 +0300

    init

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
   <NuxtLayout name="default">
-    <app-notification />
+
     <app-promotions />
     <app-popular-categories />
-    <app-bestsellers />
-    <app-special-offers />
+
   </NuxtLayout>
 </template>
 
 <script setup>
-import AppNotification from '@/components/AppNotification.vue';
+
 import AppPromotions from '@/components/AppPromotions.vue';
 import AppPopularCategories from '@/components/AppPopularCategories.vue';
-import AppBestsellers from '@/components/AppBestsellers.vue';
-import AppSpecialOffers from '@/components/AppSpecialOffers.vue';
+
 </script>
