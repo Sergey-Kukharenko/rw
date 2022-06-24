5b995b27510dc20a351c10aa4787a87785de81ea
Sergey Kukharenko <iamksergey@mail.ru>
2022-06-24 04:14:15 +0300

    init

--- data/bestsellers.js
+++ data/bestsellers.js
@@ -51,6 +51,51 @@ export default {
         status: 'sale',
         percent: '-30%'
       }
-    }
+    },
+
+    {
+      title: 'Bouquet of 151 roses',
+      'img': 'https://images.unsplash.com/photo-1582794543139-8ac9cb0f7b11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+      rating: 4.95,
+      like: false,
+      price: 52.76,
+      sale: {
+        status: 'sale',
+        percent: '-30%'
+      }
+    },
+    {
+      title: 'Bouquet of 201 roses',
+      'img': 'https://images.unsplash.com/photo-1526047932273-341f2a7631f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+      rating: 4.96,
+      like: false,
+      price: 52.76,
+      sale: {
+        status: 'sale',
+        percent: '-30%'
+      }
+    },
+    {
+      title: 'Bouquet of 251 roses',
+      'img': 'https://images.unsplash.com/photo-1476994230281-1448088947db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZsb3dlcnN8ZW58MHx8MHx8&w=1000&q=80',
+      rating: 4.98,
+      like: false,
+      price: 52.76,
+      sale: {
+        status: 'sale',
+        percent: '-30%'
+      }
+    },
+    {
+      title: 'Bouquet of 291 roses',
+      'img': 'https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZsb3dlcnN8ZW58MHx8MHx8&w=1000&q=80',
+      rating: 5.00,
+      like: false,
+      price: 52.76,
+      sale: {
+        status: 'sale',
+        percent: '-30%'
+      }
+    },
   ]
 }
