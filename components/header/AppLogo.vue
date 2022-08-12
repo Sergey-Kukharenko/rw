44883d09c91e448315e6c23dc11ebab832d91898
Sergey Kukharenko <iamksergey@mail.ru>
2022-08-12 16:57:43 +0300

    init

--- components/header/AppLogo.vue
+++ components/header/AppLogo.vue
@@ -1,27 +1,30 @@
 <template>
-  <a class="logo">FLOR</a>
+  <a class="logo">
+    <img src="/images/logo.jpg" class="logo__img" alt="logo">
+  </a>
 </template>
-
-<script setup>
-
-const isMobile = useIsMobile()
-</script>
 
 <style lang="scss" scoped>
 .logo {
-  font-family: $lora;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 32px;
-  line-height: 0.7444;
-  text-align: center;
-  letter-spacing: 0.32em;
-  cursor: pointer;
+  display: block;
+
 
   @include lt-md {
-    order: 0;
-    padding: 8px;
     margin-right: auto;
+  }
+
+  &__img{
+    display: block;
+
+    @include gt-sm {
+      max-width: 154px;
+      margin-left: -6px;
+    }
+
+    @include lt-md {
+      max-width: 111.22px;
+      margin-left: 10px;
+    }
   }
 }
 </style>
