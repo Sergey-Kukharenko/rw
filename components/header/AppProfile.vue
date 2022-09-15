20cc676c0ba6fe2a3b4bb6e45dfa2787de3ff730
Sergey Kukharenko <iamksergey@mail.ru>
2022-09-15 13:05:12 +0300

    init

--- components/header/AppProfile.vue
+++ components/header/AppProfile.vue
@@ -2,15 +2,11 @@
   <div class="profile">
     <app-dropdown :options="options">
       <template #button>
-        <div class="card">
-          <div class="card__figure">
-            <SvgSprite symbol="profile" class="card__icon" />
-          </div>
-          <div class="card__figcaption">Log in / Register</div>
-        </div>
+        <app-profile-button :user="user" />
       </template>
       <template #dropdown>
-        <app-profile-preview :user="user" />
+        <app-profile-preview v-if="user.authorized" :user="user" />
+        <div v-else>Here is Form</div>
       </template>
     </app-dropdown>
   </div>
@@ -18,7 +14,9 @@
 
 <script setup>
 import dataUser from '@/data/user'
+
 import AppDropdown from '@/components/shared/AppDropdown.vue'
+import AppProfileButton from '@/components/header/profile/AppProfileButton.vue'
 import AppProfilePreview from '@/components/header/profile/AppProfilePreview.vue'
 
 const user = ref(dataUser)
@@ -29,77 +27,3 @@ const options = {
   right: 0
 }
 </script>
-
-<style lang="scss" scoped>
-.card {
-  cursor: pointer;
-
-  @include lt-md {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  &__figure {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    @include gt-sm {
-      width: 28px;
-      height: 28px;
-      margin: 0 auto;
-    }
-
-    @include lt-md {
-      width: 32px;
-      height: 32px;
-      background: $bg-grey;
-      border-radius: 50%;
-    }
-  }
-
-  &__icon {
-    fill: $color-light-grey;
-
-    @include gt-sm {
-      width: 26.45px;
-      height: 24px;
-    }
-
-    @include lt-md {
-      width: 12px;
-      height: 12px;
-    }
-  }
-
-  &__figcaption {
-    text-align: center;
-    color: $color-white-grey;
-
-    @include gt-sm {
-      font-family: $golos-medium;
-      font-size: 14px;
-      line-height: 16px;
-      text-align: center;
-      margin-top: 7px;
-    }
-
-    @include lt-md {
-      font-family: $golos-regular;
-      font-size: 12px;
-      line-height: 16px;
-      margin-left: 8px;
-    }
-  }
-
-  &:hover {
-    opacity: 0.75;
-  }
-}
-
-.profile__dropdown {
-  top: 0;
-  right: 0;
-}
-</style>
---
+++ components/header/profile/AppProfileButton.vue
@@ -0,0 +1,116 @@
+<template>
+  <div :class="classNames">
+    <div class="profile-button__figure">
+      <template v-if="isAuth">
+        {{ char }}
+      </template>
+      <SvgSprite v-else symbol="profile" class="profile-button__icon" />
+    </div>
+    <div class="profile-button__figcaption">{{ getInfo }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const isAuth = computed(() => props.user.authorized)
+const char = computed(() => props.user.fullName.substring(0, 1))
+const getInfo = computed(() =>
+  isAuth.value ? props.user.fullName : 'Log in / Register'
+)
+
+const classNames = computed(() => [
+  useToggleClassName(isAuth.value, 'profile-button', 'active')
+])
+</script>
+
+<style lang="scss" scoped>
+.profile-button {
+  cursor: pointer;
+
+  @include lt-md {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__figure {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @include gt-sm {
+      width: 28px;
+      height: 28px;
+      margin: 0 auto;
+    }
+
+    @include lt-md {
+      width: 32px;
+      height: 32px;
+      background: $bg-grey;
+      border-radius: 50%;
+    }
+  }
+
+  &__icon {
+    fill: $color-light-grey;
+
+    @include gt-sm {
+      width: 26.45px;
+      height: 24px;
+    }
+
+    @include lt-md {
+      width: 12px;
+      height: 12px;
+    }
+  }
+
+  &__figcaption {
+    text-align: center;
+    color: $color-white-grey;
+
+    @include gt-sm {
+      font-family: $golos-medium;
+      font-size: 14px;
+      line-height: 16px;
+      text-align: center;
+      margin-top: 7px;
+    }
+
+    @include lt-md {
+      font-family: $golos-regular;
+      font-size: 12px;
+      line-height: 16px;
+      margin-left: 8px;
+    }
+  }
+
+  &:hover {
+    opacity: 0.75;
+  }
+
+  &--active {
+    & .profile-button__figure {
+      width: 24px;
+      height: 24px;
+      font-family: $golos-bold;
+      font-size: 16px;
+      line-height: 16px;
+      color: #fff;
+      background: #e8929d;
+      border-radius: 8px;
+    }
+
+    & .profile-button__figcaption {
+      color: $color-dark-grey;
+    }
+  }
+}
+</style>
