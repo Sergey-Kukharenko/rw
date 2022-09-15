15ecc918cbe2f0066bab3317b6291c2e870f5e2a
Sergey Kukharenko <iamksergey@mail.ru>
2022-09-15 15:31:52 +0300

    init

--- components/header/AppProfile.vue
+++ components/header/AppProfile.vue
@@ -6,12 +6,7 @@
       </template>
       <template #dropdown>
         <app-profile-preview v-if="user.authorized" :user="user" />
-        <div v-else class="profile-form">
-          <h1>Will be form ...</h1>
-          <button @click="store.logIn()" class="profile-form__button">
-            log in
-          </button>
-        </div>
+        <app-profile-form v-else />
       </template>
     </app-dropdown>
   </div>
@@ -21,30 +16,14 @@
 import AppDropdown from '@/components/shared/AppDropdown.vue'
 import AppProfileButton from '@/components/header/profile/AppProfileButton.vue'
 import AppProfilePreview from '@/components/header/profile/AppProfilePreview.vue'
+import AppProfileForm from '@/components/header/profile/AppProfileForm.vue'
 
 import { useUserStore } from '@/stores/useUserStore'
 
 const store = useUserStore()
 const user = computed(() => store.user)
-const char = computed(() => user.value.fullName.substring(0, 1))
-
 const options = {
   top: '-20px',
   right: 0
 }
 </script>
-
-<style lang="scss" scoped>
-.profile-form {
-  height: 290px;
-  width: 320px;
-  padding: 18px 20px;
-
-  &__button {
-    color: #fff;
-    padding: 0.5em;
-    background: #ccc;
-    border-radius: 8px;
-  }
-}
-</style>
---
+++ components/header/profile/AppProfileForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile-form">
+    <h1>Will be form ...</h1>
+    <button @click="store.logIn()" class="profile-form__button">log in</button>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/useUserStore'
+
+const store = useUserStore()
+const user = computed(() => store.user)
+</script>
+
+<style lang="scss" scoped>
+.profile-form {
+  height: 290px;
+  width: 320px;
+  padding: 18px 20px;
+
+  &__button {
+    color: #fff;
+    padding: 0.5em;
+    background: #ccc;
+    border-radius: 8px;
+  }
+}
+</style>
