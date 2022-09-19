b40bb425c0f6f29d85ba3ae21cf8cb3067080482
Sergey Kukharenko <iamksergey@mail.ru>
2022-09-19 17:35:41 +0300

    init

---
+++ components/header/search/AppList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="list">
+    <div class="list__item" v-for="(item, idx) in props.list" :key="idx">
+      <span @click="add(item)">{{ item }}</span>
+      <SvgSprite
+        v-if="hasRemoveBtn"
+        symbol="close"
+        class="button"
+        @click="remove(item)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/useUserStore'
+
+const props = defineProps({
+  list: {
+    type: Array,
+    default: () => []
+  },
+
+  hasRemoveBtn: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const store = useUserStore()
+const emit = defineEmits(['addItem', 'removeItem'])
+
+const add = (item) => {
+  emit('addItem', item)
+}
+
+const remove = (item) => {
+  emit('removeItem', item)
+}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  &__item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: $golos-regular;
+    font-size: 16px;
+    line-height: 24px;
+    color: $color-dark-grey;
+    padding: 4px 8px;
+    user-select: none;
+    cursor: pointer;
+
+    &:hover {
+      background: $bg-grey;
+      border-radius: 10px;
+    }
+  }
+}
+
+.button {
+  width: 10px;
+  height: 10px;
+  padding: 4px;
+  margin-left: 10px;
+  opacity: 0;
+  transform: scale(0.8);
+  transition: opacity 0.2s ease-out 0s, transform 0.2s ease-out 0s;
+
+  &:hover {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
--- components/header/search/AppSearch.vue
+++ components/header/search/AppSearch.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="search">
+  <div class="search" v-click-outside="onHideSearchHistory">
     <div class="layout">
-      <form class="search-form" @submit.prevent="store.addToHistory(query)">
+      <form class="search-form" @submit.prevent="onSubmit">
         <input
           autocomplete="off"
           type="text"
@@ -9,23 +9,22 @@
           placeholder="Search for the best bouquet"
           class="input"
           v-model="query"
+          @focus="onShowSearchHistory"
         />
         <app-button theme="search" size="lg">Search</app-button>
       </form>
 
-      <div class="search__section section">
+      <div class="section" v-show="isSearchHistory">
         <div class="section__title">Search history</div>
         <div class="section__list">
-          <div class="list">
-            <div
-              class="list__item"
-              v-for="(item, idx) in searchHistory"
-              :key="idx"
-              @click="store.removeFromHistory(item)"
-            >
-              {{ item }}
-            </div>
-          </div>
+          <app-list :list="searchHistory" hasRemoveBtn @removeItem="onRemoveItem" />
+        </div>
+      </div>
+
+      <div class="section" v-show="isSearchResult">
+        <div class="section__title">Categories</div>
+        <div class="section__list">
+          <app-list :list="filteredList" @addItem="onAddItem" />
         </div>
       </div>
     </div>
@@ -33,13 +32,52 @@
 </template>
 
 <script setup>
+import siteData from '@/data/site-data'
 import { useUserStore } from '@/stores/useUserStore'
 import AppButton from '@/components/shared/AppButton.vue'
+import AppList from './AppList.vue'
+
+const data = ref(siteData)
 
 const query = ref('')
+const filteredList = computed(() =>
+  data.value.filter((item) =>
+    item.toLowerCase().includes(query.value.toLowerCase())
+  )
+)
+
+const isSearchResult = computed(
+  () => filteredList.value.length > 0 && query.value
+)
 
 const store = useUserStore()
 const searchHistory = computed(() => store.user.searchHistory)
+
+const showSearchHistory = ref(false)
+const isSearchHistory = computed(
+  () =>
+    searchHistory.value.length > 0 && showSearchHistory.value && !query.value
+)
+
+const onShowSearchHistory = () => (showSearchHistory.value = true)
+const onHideSearchHistory = () => (showSearchHistory.value = false)
+const clearQuery = () => query.value = ''
+
+const onSubmit = () => {
+  store.addToHistory(query.value)
+  clearQuery()
+}
+
+const onRemoveItem = (payload) => {
+  store.removeFromHistory(payload)
+}
+
+const onAddItem = (payload) => {
+  store.addToHistory(payload)
+  clearQuery()
+}
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -52,10 +90,6 @@ const searchHistory = computed(() => store.user.searchHistory)
   min-height: 376px;
   border-radius: 0 0 24px 24px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.04);
-
-  &__section {
-    margin: 16px 0;
-  }
 }
 
 .search-form {
@@ -85,6 +119,7 @@ const searchHistory = computed(() => store.user.searchHistory)
 
 .section {
   display: flex;
+  margin: 16px 0;
 
   &__title {
     width: 164px;
@@ -94,22 +129,6 @@ const searchHistory = computed(() => store.user.searchHistory)
     line-height: 24px;
     color: $color-white-grey;
     letter-spacing: -0.01em;
-  }
-}
-
-.list {
-  &__item {
-    font-family: $golos-regular;
-    font-size: 16px;
-    line-height: 24px;
-    color: $color-dark-grey;
-    padding: 4px 8px;
-    cursor: pointer;
-
-    &:hover {
-      background: $bg-grey;
-      border-radius: 10px;
-    }
   }
 }
 </style>
